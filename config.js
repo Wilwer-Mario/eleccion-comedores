93536f0ead02f50f793d023f5b4a394acdd95cdf
Lu Sheng <wilwer.mario@gmail.com>
2026-09-04 21:12:56 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// =========================================================================
+// config.js — Llaves de conexión a Firebase
+// =========================================================================
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAvvJgOZtJsJZEHF38QE5rF-3qczD6y2Ig",
+  authDomain: "comedores-mina.firebaseapp.com",
+  databaseURL: "https://comedores-mina-default-rtdb.firebaseio.com",
+  projectId: "comedores-mina",
+  storageBucket: "comedores-mina.firebasestorage.app",
+  messagingSenderId: "305123003691",
+  appId: "1:305123003691:web:7e834035114394a7e2496e"
+};
